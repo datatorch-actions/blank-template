fe403e4f019614db40c2826afb9b5e527e5b242e
Michael Nguyen <ninjeezy@gmail.com>
2023-02-28 17:45:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import datatorch
 
 if __name__ == "__main__":
     inputs = datatorch.get_inputs()
-    url: string = inputs.get('url')
+    url = inputs.get('url')
     datatorch.set_output('completed', true)
