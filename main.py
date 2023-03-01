a21bcf4949171545169185c455a1ae55b9bb1887
Michael Nguyen <ninjeezy@gmail.com>
2023-03-01 10:42:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import sys
 import datatorch
 
+from pytube import YouTube
+
 if __name__ == "__main__":
     inputs = datatorch.get_inputs()
     url = inputs.get('url')
+
+    youtubeObject = YouTube(url)
+    youtubeObject  = youtubeObject.streams.get_highest_resolution()
+
+    try:
+        youtubeObject.download()
+    except:
+        print("Error with downloading YouTube video.")
+        datatorch.set_output('completed', False)
+        
+    print("YouTube download successful.")
     datatorch.set_output('completed', True)
